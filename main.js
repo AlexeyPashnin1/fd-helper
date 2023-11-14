863099fb4d17e373832fefb1d1c5673c93e58f31
Alexey <109278274+AlexeyPashnin1@users.noreply.github.com>
2023-11-14 15:14:08 +0500

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function addSysClearQuotes() {
             document.querySelector('.surveys .ticket-editor__action').click();
             console.log('WOW')
     }
-    }, 0)
+    }, 500)
     if (!document.querySelector('.conversation-dotted-loader')) {
         document.querySelectorAll('div[data-test-id="conversation-wrapper"], .requestor-wrap').forEach(function (e) {
             let noteID = e.getAttribute('data-album').replace('note_', '').replace('ticket_', '');
