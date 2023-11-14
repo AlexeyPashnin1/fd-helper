bbb63d19244e0f9cc12774a10f8329ed44d5558b
Alexey <109278274+AlexeyPashnin1@users.noreply.github.com>
2023-11-14 15:13:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -25,16 +25,16 @@ function clearQuotes() {
 }
 
 function addSysClearQuotes() {
+    setTimeout(function() {
     if (
         document.querySelector('.surveys:not(.fixed) .ticket-editor__action')
         && !document.querySelector('.surveys .ticket-editor__action').classList.contains('ticket-editor__action--active')
     ) {
-        document.querySelector('.surveys').classList.add('fixed');
-        setTimeout(function() {
+            document.querySelector('.surveys').classList.add('fixed');
             document.querySelector('.surveys .ticket-editor__action').click();
             console.log('WOW')
-        }, 0)
     }
+    }, 0)
     if (!document.querySelector('.conversation-dotted-loader')) {
         document.querySelectorAll('div[data-test-id="conversation-wrapper"], .requestor-wrap').forEach(function (e) {
             let noteID = e.getAttribute('data-album').replace('note_', '').replace('ticket_', '');
